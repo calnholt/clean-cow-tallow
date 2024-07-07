9e4f681ba4c17fc8ea1b55f5151bc26f420ac219
DESKTOP-8BUND66\Calvin <calnholt@gmail.com>
2024-07-07 15:00:42 -0400

    about and info sections

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function main() {
   window.addEventListener('scroll', function () {
     const header = document.getElementsByClassName('logo')[0];
-    if (window.scrollY > 50) {
+    if (window.scrollY > 250) {
         header.classList.add('shrink');
     } else {
         header.classList.remove('shrink');
