a2c543c65468108a93fb84d934dd9ee5bd64e7c7
DESKTOP-8BUND66\Calvin <calnholt@gmail.com>
2024-07-07 20:38:08 -0400

    instagram

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 function main() {
-  window.addEventListener('scroll', function () {
-    const header = document.getElementsByClassName('logo')[0];
-    if (window.scrollY > 220) {
-        header.classList.add('shrink');
-    } else {
-        header.classList.remove('shrink');
-    }
-});
+    window.addEventListener('scroll', function () {
+        const header = document.getElementsByClassName('logo')[0];
+        if (window.scrollY > 220) {
+            header.classList.add('shrink');
+        } else {
+            header.classList.remove('shrink');
+        }
+    });
+}
+
+function goToInstagram() {
+    window.open('https://www.instagram.com/cleancowtallow?igsh=cXZiMjMxb2p0MTc%3D&utm_source=qr', '_blank').focus();
+}
+function goToEtsy() {
+    window.open('http://cleancowtallow.etsy.com/', '_blank').focus();
 }
