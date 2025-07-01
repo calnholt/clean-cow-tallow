22c6f69453819a2b9bdd10c67cfa7c9d9a04f0f6
Calvin Holt <>
2025-07-01 18:39:01 -0400

    july 2025 changes, p1

--- constants.js
+++ constants.js
@@ -14,11 +14,11 @@ const FAQ  = {
   CONTENTS: [
     `In terms of skin care products, "clean" means free of toxins including parabens, fragrances, phthalates, sulfates, and artificial colors.`,
     `Tallow is beef fat that has been rendered and filtered for purity. It has been used for many centuries to make balms and soaps due to its accessibility and high nutrient content.`,
-    `Pure grass-fed beef tallow is packed with vitamins A, D, E and K, and contains fatty acids for better moisturizing. It also provides an emollient skin barrier for extra protection, and won't clog pores because it works a lot like the oil our skin produces naturally. It is an all-around powerhouse for nontoxic and effective skin care.`,
-    `Clean Cow Tallow is a small, mom-owned business that is dedicated to using the best quality ingredients available. Our tallow is made from pasture-raised and exclusively grass-fed cows raised on Amish farms, ensuring a nourishing and nutrient-dense product. The honey we use is always raw to maintain its natural properties that are so helpful for skin care. And our olive oil is organic, cold-pressed, and extra virgin. All of our essential oil options are also from organic sources.`,
+    `Pure grass-fed and pasture-raised beef tallow is packed with vitamins A, D, E and K, and contains fatty acids for better moisturizing. It also provides an emollient skin barrier for extra protection, and won't clog pores because it works a lot like the oil our skin produces naturally.  It is an all-around powerhouse for nontoxic and effective skin care.`,
+    `Clean Cow Tallow is a small, mom-owned business that is dedicated to using the best quality ingredients available. Our tallow is made from pasture-raised and exclusively grass-fed cows raised on Amish farms, ensuring a nourishing and nutrient-dense product. The honey we use is always raw to maintain its natural properties that are so helpful for skin care. Our olive oil is organic, cold-pressed, and extra virgin. And the beeswax we use is 100% pure, from a small family farm in Ohio. All of our essential oil options are also from organic sources.`,
     `Clean Cow Tallow balm can be used just about anywhere you need extra moisturization and protection. Some uses include lip balm, facial moisturizer, sunburn balm, spot treatment for dry patches, scars, or burns, and diaper balm. Our whipped tallow cream is more easily spreadable and is perfect for use as a body lotion or hand cream.\n\nOur tallow products absorb best when only a little is used. You'll find that a little goes a long way.`,
     `As often as needed!`,
-    `We would recommend trying our unscented balm or cream. Unless you have a specific allergy to beef, honey, or olives, our tallow balm is naturally gentle on the skin.`,
+    `We would recommend trying our unscented balm or cream. Unless you have a specific allergy to beef, honey, olives,or bee propolis, our tallow balm is naturally gentle on the skin.`,
     `There is no clear evidence that tallow will increase sunburn risk. Clean Cow Tallow products aren't intended to be used as tanning oil or sunscreen.`,
     `It shouldn't! The fatty acids in tallow act similarly to our own natural skin oils. Tallow also provides an emollient barrier that helps keep dirt out and moisture in.`,
     `Tallow is very shelf-stable when properly stored. We recommend storing your tallow in an air-tight container away from light and heat. Doing so should make your tallow last for up to a year, though you'll probably finish the jar long before then. 😊`,
@@ -29,6 +29,6 @@ const IMAGES = [
   "./images/photo-3.jpeg",
   "./images/photo-2.jpeg",
   "./images/photo-1.jpeg",
-  "./images/product-1.jpeg",
-  "./images/product-2.jpeg",
+  "./images/product-1.png",
+  "./images/product-2.png",
 ];
